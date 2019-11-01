6a0d3c0001c056eea36f8ded28545fb04d621b71
Flávio Ribeiro <email@flavioribeiro.com>
2019-10-31 20:15:14 -0400

    use forked version of go-dash to parse our manifests

--- handlers.go
+++ handlers.go
@@ -4,22 +4,38 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
+
+	"github.com/cbsinteractive/go-dash/mpd"
 )
 
 // LoadHandler loads the handler for all the requests
 func LoadHandler(c Config) http.Handler {
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
+		logger := c.GetLogger()
 		manifestURL := "https://vod-gcs-cedexis.cbsaavideo.com/intl_vms/2019/10/17/1625066563842/123002_cenc_dash/stream.mpd"
 
 		client := c.Client.New()
 		resp, err := client.Get(manifestURL)
 		if err != nil {
-			fmt.Fprintf(w, "failed fetching url")
+			logger.WithError(err).Fatal(w, "failed fetching url")
+			return
 		}
 
 		buf := new(bytes.Buffer)
 		buf.ReadFrom(resp.Body)
-		fmt.Fprintf(w, buf.String())
+
+		manifest, err := mpd.ReadFromString(buf.String())
+		if err != nil {
+			logger.WithError(err).Fatal(w, "failed to parse mpd")
+		}
+
+		newManifest, err := manifest.WriteToString()
+		if err != nil {
+			logger.WithError(err).Fatal(w, "failed to generate mpd")
+		}
+
+		fmt.Fprintf(w, newManifest)
 	})
 }
