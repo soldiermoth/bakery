724897fe2e9db7f6709d9b2f6a9a4c2f2ab62128
Flávio Ribeiro <email@flavioribeiro.com>
2019-11-28 13:46:13 -0500

    main: run application in lambda function

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/cbsinteractive/bakery/config"
 	"github.com/cbsinteractive/bakery/handlers"
+
+	"github.com/akrylysov/algnhsa"
 )
 
 func main() {
@@ -16,16 +18,23 @@ func main() {
 	}
 
 	logger := c.GetLogger()
-
-	listener, err := net.Listen("tcp", c.Listen)
-	if err != nil {
-		logger.WithError(err).Fatal("failed to initialize listener")
-	}
-
-	logger.Infof("Starting Bakery on %s", listener.Addr())
 	handler := handlers.LoadHandler(c)
-	err = http.Serve(listener, handler)
-	if err != nil {
-		logger.WithError(err).Fatal("failed to initialize server")
+
+	_, isLambda := os.LookupEnv("AWS_LAMBDA_FUNCTION_NAME")
+
+	// check if it's running on lambda environment or not
+	if isLambda {
+		algnhsa.ListenAndServe(handler, nil)
+	} else {
+		listener, err := net.Listen("tcp", c.Listen)
+		if err != nil {
+			logger.WithError(err).Fatal("failed to initialize listener")
+		}
+
+		logger.Infof("Starting Bakery on %s", listener.Addr())
+		err = http.Serve(listener, handler)
+		if err != nil {
+			logger.WithError(err).Fatal("failed to initialize server")
+		}
 	}
 }
